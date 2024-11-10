a552665cbf751464742b2b7e54831d89ca2094b5
akash2061 <lucifermorningstar2061@gmail.com>
2024-11-10 23:56:51 +0530

    Routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import RoutesConf from "./routes/RoutesConf";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  // const [greetMsg, setGreetMsg] = useState("");
+  // const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  // async function greet() {
+  //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  //   setGreetMsg(await invoke("greet", { name }));
+  // }
 
   return (
-    <div className="bg-slate-600">
-      <h1>Foo...</h1>
-    </div>
+    <Router>
+      <Routes>
+        {RoutesConf}
+      </Routes>
+    </Router>
   );
 }
 
