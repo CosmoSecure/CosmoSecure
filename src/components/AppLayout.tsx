e95198675c5ce53c30272382abcb4c03868d90fa
akash2061 <lucifermorningstar2061@gmail.com>
2024-11-11 19:08:22 +0530

    Updated AppLayout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,40 +1,22 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Decor from '../components/Decor';
+import Navigate from './Navigate';
 
 const AppLayout: React.FC = () => {
     return (
-        <div className='flex flex-col h-screen'>
-            {/* Header Section */}
-            <header className='bg-gray-800 text-white p-4'>
-                <h1>Decoration</h1>
-            </header>
-
-            <div className='flex flex-1 pt-1'>
-                {/* Sidebar Section */}
-                <nav className='w-64 bg-gray-200 p-4'>
-                    <ul className='space-y-4'>
-                        <li>
-                            <Link to="/home" className="text-blue-600 hover:underline">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about" className="text-blue-600 hover:underline">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact" className="text-blue-600 hover:underline">Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                {/* Main Content Section */}
-                <div className='flex-1 pl-1 overflow-auto'>
-                    <main className='flex-1 h-full'>
-                        {/* The Outlet renders the current route's page here */}
-                        <Outlet />
-                    </main>
-                </div>
+        <div className="flex h-screen">
+            <div className='m-1'>
+                <Navigate />
+            </div>
+            <div className="flex-1 flex flex-col m-1 ml-0">
+                <Decor />
+                <main className="flex-1 overflow-auto bg-blue-100 mt-1 rounded-md">
+                    <Outlet />
+                </main>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default AppLayout
+export default AppLayout;
--- src/components/AppLayout2.tsx
+++ src/components/AppLayout2.tsx
@@ -1,38 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Decor from '../components/Decor';
+import Navigate from './Navigate';
 
-const AppLayout2: React.FC = () => {
+const AppLayout: React.FC = () => {
     return (
-        <div className='flex flex-col h-screen'>
-
-            <div className='flex flex-1'>
-                <nav className='w-64 bg-gray-200 p-4'>
-                    <ul className='space-y-4'>
-                        <li>
-                            <a href="/" className="text-blue-600 hover:underline">Home</a>
-                        </li>
-                        <li>
-                            <a href="/about" className="text-blue-600 hover:underline">About</a>
-                        </li>
-                        <li>
-                            <a href="/contact" className="text-blue-600 hover:underline">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
-
-                <div className='flex-1 overflow-auto'>
-                    <header className='bg-gray-800 text-white p-4'>
-                        <h1>Decoration</h1>
-                    </header>
-                    <main className='flex-1 p-6 overflow-auto'>
-
-                        {/* <Outlet /> */}
-                        <h1>Main</h1>
-                    </main>
-                </div>
+        <div className="flex h-screen">
+            <div className='m-1'>
+                <Navigate />
+            </div>
+            <div className="flex-1 flex flex-col m-1 ml-0">
+                <Decor />
+                <main className="flex-1 overflow-auto bg-blue-100 mt-1 rounded-md">
+                    <Outlet />
+                </main>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default AppLayout2
+export default AppLayout;
