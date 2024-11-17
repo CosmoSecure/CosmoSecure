605e8459ae02fe58ef5f138e8537b248f2cb3017
akash2061 <lucifermorningstar2061@gmail.com>
2024-11-17 22:12:42 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import { BrowserRouter as Router, Routes } from 'react-router-dom'
 import RoutesConf from "./routes/RoutesConf";
 import { getCurrentWindow } from "@tauri-apps/api/window";
 
 function App() {
-  // const [greetMsg, setGreetMsg] = useState("");
-  // const [name, setName] = useState("");
-
-  // async function greet() {
-  //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  //   setGreetMsg(await invoke("greet", { name }));
-  // }
-
+  getCurrentWindow().setContentProtected(true);
   return (
-    getCurrentWindow().setContentProtected(true),
     <Router>
       <Routes>
         {RoutesConf}
