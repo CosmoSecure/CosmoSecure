5d8655e0df73aceba640ddd9bb47bcd5cdee9db0
akash2061 <lucifermorningstar2061@gmail.com>
2024-11-24 18:35:01 +0530

    Improved Login/Signup Layout

--- src/components/auth/Auth_page.tsx
+++ src/components/auth/Auth_page.tsx
@@ -5,7 +5,7 @@ import Decor from '../Decor';
 const Auth_page: React.FC = () => {
     return (
         <div className="flex h-screen">
-            <div className="flex-1 flex flex-col m-1 ml-0">
+            <div className="flex-1 flex flex-col m-1">
                 <Decor />
                 <main className="flex-1 overflow-auto bg-blue-100 mt-1 rounded-md">
                     <Outlet />
