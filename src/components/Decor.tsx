6caa624e66c6e717588bb4db5a32cc435033583b
akash2061 <lucifermorningstar2061@gmail.com>
2024-11-26 21:24:04 +0530

    Add logo to Decor component and improve layout structure

--- src/components/Decor.tsx
+++ src/components/Decor.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { getCurrentWindow } from '@tauri-apps/api/window';
+import Logo from '../assets/Logo.png'; // Adjust the path as necessary
 
 const Decor: React.FC = () => {
 
@@ -18,13 +19,13 @@ const Decor: React.FC = () => {
 
     return (
         <div data-tauri-drag-region className="bg-gray-800 text-white p-4 h-12 w-full text-center rounded-md flex items-center justify-between">
-            <div data-tauri-drag-region>
-                win-name
+            <div data-tauri-drag-region className="flex items-center justify-start">
+                <img data-tauri-drag-region src={Logo} alt="Logo" className="h-20 mr-2" />
             </div>
-            <div>
-                search
+            <div data-tauri-drag-region className='flex-grow flex justify-center'>
+                Window Name
             </div>
-            <div className='flex justify-end text-sm'>
+            <div className='flex justify-end text-sm ml-[124px]'>
                 <button className="py-1 ml-2 transition-all duration-300 ease-in-out hover:scale-110" onClick={minimizeWebview}>🟡</button>
                 <button className="py-1 ml-2 transition-all duration-300 ease-in-out hover:scale-110" onClick={maximizeWebview}>🟣</button>
                 <button className="py-1 ml-2 transition-all duration-300 ease-in-out hover:scale-110" onClick={closeWebview}>🔴</button>
