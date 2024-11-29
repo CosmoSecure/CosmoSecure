9a0aba87a8d67f69983bea6d8488f5cad67d7ec0
akash2061 <lucifermorningstar2061@gmail.com>
2024-11-30 00:56:55 +0530

    Refactor token management to use environment variables and implement key derivation

--- src-tauri/src/db/token.rs
+++ src-tauri/src/db/token.rs
@@ -1,10 +1,10 @@
-use dotenv::dotenv;
+use crate::env_var::{get_env_key, get_env_vars};
+// use crate::secure::encrypt;
+use crate::secure::decrypt;
 use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
 use serde::{Deserialize, Serialize};
-use std::{
-    env,
-    time::{Duration, SystemTime},
-};
+use sha2::{Digest, Sha256};
+use std::time::{Duration, SystemTime};
 
 #[derive(Serialize, Deserialize)]
 struct Claims {
@@ -13,9 +13,19 @@ struct Claims {
 }
 
 pub fn generate_token(user_id: &str) -> Result<String, String> {
-    dotenv().ok();
+    let env_vars = get_env_vars();
 
-    let token_secret = env::var("TOKEN_SECRET").map_err(|e| e.to_string())?;
+    let token_var = env_vars
+        .get("TOKEN_SECRET")
+        .ok_or("TOKEN_SECRET not found")?;
+    let key = get_env_key().expect("KEY must be set");
+    let sec_key = derive_key(key);
+
+    // let token = encrypt(token_var, &sec_key);
+    // println!("Encoded - Token: {:?} \n", token.unwrap());
+    let token_secret = decrypt(token_var, &sec_key);
+    let token_sec = token_secret.unwrap();
+
     let expiration = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .map_err(|e| e.to_string())? // Handle potential errors
@@ -27,8 +37,32 @@ pub fn generate_token(user_id: &str) -> Result<String, String> {
     };
 
     let header = Header::new(Algorithm::HS256);
-    let encoding_key = EncodingKey::from_secret(token_secret.as_ref());
+    let encoding_key = EncodingKey::from_secret(token_sec.as_ref());
 
     // Encode the claims into a JWT token
     encode(&header, &claims, &encoding_key).map_err(|e| e.to_string()) // Convert errors to String
+}
+
+// Derive the key using SHA-256
+fn derive_key(input_key: &str) -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    hasher.update(input_key.as_bytes());
+    let result = hasher.finalize();
+    let mut key = [0u8; 32];
+    key.copy_from_slice(&result[..32]);
+    key
+}
+
+use crate::config::{delete_config, load_from_config, save_to_config};
+
+pub fn save_token(token: &str, user: &str) {
+    save_to_config(token, user);
+}
+
+pub fn load_token() -> (String, String) {
+    load_from_config()
+}
+
+pub fn delete_token() {
+    delete_config();
 }
