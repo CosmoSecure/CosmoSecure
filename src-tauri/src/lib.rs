8d74ace36420f6cd5c0fbc8e6e6a2f52c2a4be21
akash2061 <lucifermorningstar2061@gmail.com>
2024-11-30 00:54:13 +0530

    Add token management commands for saving, loading, and deleting tokens

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,26 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use crate::db::db_connect::{authenticate_user, check_username_availability, tauri_add_user};
+use crate::db::token;
 
+pub mod config;
 mod db;
+pub mod env_var;
 mod secure;
+
+#[tauri::command]
+fn save_token_command(token: String, user: String) {
+    token::save_token(&token, &user);
+}
+
+#[tauri::command]
+fn load_token_command() -> (String, String) {
+    token::load_token()
+}
+
+#[tauri::command]
+fn delete_token_command() {
+    token::delete_token();
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub async fn run() {
@@ -21,7 +39,10 @@ pub async fn run() {
         .invoke_handler(tauri::generate_handler![
             tauri_add_user,
             authenticate_user,
-            check_username_availability
+            check_username_availability,
+            save_token_command,
+            load_token_command,
+            delete_token_command
         ])
         .setup(|_app| {
             // Use an asynchronous runtime to run the database connection
