2ace787b43f12435f61cc75a9b518dcc3223ef7d
akash2061 <lucifermorningstar2061@gmail.com>
2024-11-30 23:53:07 +0530

    Refactor environment variable definitions for clarity and maintainability

--- src-tauri/src/env_var.rs
+++ src-tauri/src/env_var.rs
@@ -9,9 +9,15 @@ pub fn get_env_vars() -> HashMap<&'static str, &'static str> {
     // Add your environment variables here
     // mongodb://localhost:27017 is the default MongoDB URI
     // env_vars.insert("MONGO_URI", "mongodb://localhost:27017");
-    env_vars.insert("MONGO_URI", "lM7PEhFes5wh22i8O0wK2WW9smXRsQ/+hOjnQ7Xzs39spyAl1zHtdIM=");
-    env_vars.insert("TOKEN_SECRET", "wMeWEEgJs5U7xD2ybExXiWn2/m/S5wr7ilNfY4jV+6I1YWVDB8BEpSLK688bO2LZwk3McMu+9WymMnIMm4j4mSqpnBLuXNEGHBbxOu5yBb4=");
-    env_vars.insert("KEY", "aP9s87f6gR5h4j3k5l1G0n9b8v7c6xO2");
+    env_vars.insert(
+        "MONGO_URI",
+        "lM7PEhFes5wh22i8O0wK2WW9smXRsQ/+hOjnQ7Xzs39spyAl1zHtdIM=",
+    ); // mongodb_uri hash
+    env_vars.insert(
+        "TOKEN_SECRET",
+        "wMeWEEgJs5U7xD2ybExXiWn2/m/S5wr7ilNfY4jV+6I1YWVDB8BEpSLK688bO2LZwk3McMu+9WymMnIMm4j4mSqpnBLuXNEGHBbxOu5yBb4="
+    ); // jwt_token_secret hash ka hash
+    env_vars.insert("KEY", "aP9s87f6gR5h4j3k5l1G0n9b8v7c6xO2"); // random key
 
     env_vars
 }
