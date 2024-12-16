9ecf70bac9622c1d1624ae430c2d8d600c419fc0
akash2061 <lucifermorningstar2061@gmail.com>
2024-12-16 11:07:29 +0530

    Add new asset imports for Old, Crow, and Key images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,6 @@
+import Old from './old.jpg';
+import Crow from './crow.png';
+import Key from './Key.png';
 import DecorLogo from './Logo.png';
 import Logo from './logo.png';
 import Background from './temp.jpg';
@@ -5,6 +8,10 @@ import Background from './temp.jpg';
 import Pro from './pro.jpeg';
 
 export {
+    Old,
+    Crow,
+    Key,
+
     DecorLogo,
     Logo,
     Background,
