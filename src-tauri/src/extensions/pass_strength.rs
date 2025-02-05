302769b4ea9d8ec683b50ec3f69c59695d2ff7ce
akash2061 <lucifermorningstar2061@gmail.com>
2025-02-05 22:39:36 +0530

    Enhance password strength feedback with custom messages and improved suggestions handling

--- src-tauri/src/extensions/pass_strength.rs
+++ src-tauri/src/extensions/pass_strength.rs
@@ -11,23 +11,35 @@ pub struct PasswordStrength {
 pub fn check_password_strength(password: String) -> Result<PasswordStrength, String> {
     let estimate = zxcvbn(&password, &[]);
 
-    let feedback = estimate
-        .feedback()
-        .map_or_else(
-            || "No feedback available".to_string(),
-            |f| {
+    let feedback = estimate.feedback().map_or_else(
+        || "Great Security, Greater Coffee Needed! ☕😅 [buymeacoffee.com/akash2061]".to_string(),
+        |f| {
             let warning = f
                 .warning()
-                .map_or("No warning available.".to_string(), |w| w.to_string())
+                .map_or("".to_string(), |w| w.to_string())
                 .to_string(); // Handle warning
-            let suggestions = f
+            let mut suggestions = f
                 .suggestions()
                 .iter()
                 .map(|s| s.to_string())
                 .collect::<Vec<_>>()
                 .join(" "); // Join suggestions
+
+            match suggestions.contains("Add another word or two.") {
+                true => {
+                    suggestions = suggestions
+                        .replace("Add another word or two.", "")
+                        .trim()
+                        .to_string();
+                    if suggestions.starts_with(" ") {
+                        suggestions = suggestions[1..].to_string();
+                    }
+                }
+                false => {}
+            }
             format!("{} {}", warning, suggestions).trim().to_string()
-        });
+        },
+    );
 
     Ok(PasswordStrength {
         score: estimate.score() as u8,
