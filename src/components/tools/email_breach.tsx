9fb7e96cc07d20bb9a0184ae64fd1064aed02260
akash2061 <lucifermorningstar2061@gmail.com>
2025-09-05 13:08:26 +0530

    Added BackDrop Blur

--- src/components/tools/email_breach.tsx
+++ src/components/tools/email_breach.tsx
@@ -140,6 +140,13 @@ const EmailBreach: React.FC = () => {
                 onClose={() => setIsModalOpen(false)}
                 aria-labelledby="breach-details-modal"
                 aria-describedby="breach-details-description"
+                BackdropProps={{
+                    sx: {
+                        backgroundColor: 'rgba(0, 0, 0, 0.6)',
+                        backdropFilter: 'blur(3px)',
+                        WebkitBackdropFilter: 'blur(3px)',
+                    }
+                }}
             >
                 <Box
                     className="bg-theme-accent-transparent p-6 rounded-md shadow-lg max-w-5xl mx-auto mt-10 overflow-y-auto max-h-[90vh]"
